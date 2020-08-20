547d002d9c5c8f7fa1890d37af1b0f0ff2825382
ossamuel <46551923+ossamuel@users.noreply.github.com>
2020-08-19 21:25:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from flask import Flask, render_template, request
 import numpy as np
 import pickle
 # import sklearn
+import os
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
 
 app = Flask(__name__)
 
-model = pickle.load(open('diabetic_clf_model.plk', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/', methods=["GET", "POST"])
 def main():
     if request.method == "POST":
-        age = request.form["age"]
-        bmi = request.form["bmi"]
-        bp = request.form["bp"]
-        glu = request.form["glu"]
-
+        
         int_features = [int(float(x)) for x in request.form.values()]
         final = [np.array(int_features)]
-        prediction = model.predict(final)
+        print(final)
+        prediction = model.predict_proba(final)
         
-        return render_template('index.html', success=True, age=age, bmi=bmi, bp=bp, glu=glu, pred=prediction)
+        return render_template('index.html', success=True, predict_text = 'your probability of being diabetic is  {} '.format(prediction[0][1]))
     else:
         return render_template('index.html', success=False)
 
